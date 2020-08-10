6aebebfeb2027e37dbaa9ca2fb9447288f2aa4fa
Jeff Simpson <jeff1010322@gmail.com>
2020-08-10 15:56:50 -0400

    feat(xdrTypes): check in xdrTypes.js and update instructions, remove gitignore
    Instructions updated to explain xdrTypes.js better.

---
+++ xdrTypes.js
@@ -0,0 +1,83 @@
+
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _xdrJsSerialize = _interopRequireDefault(require("xdr-js-serialize"));
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+        default: obj
+    };
+}
+
+// Namespace start example
+
+// Start typedef section
+
+exports.ID = ID;
+function ID() {
+    return new _xdrJsSerialize.default.FixedOpaque(32);
+}
+// End typedef section
+
+// Start struct section
+exports.Bar = Bar;
+function Bar() {
+    return new _xdrJsSerialize.default.Struct(
+        ["id",],
+        [ID(),]
+    )
+}
+
+// End struct section
+
+// Start enum section
+
+
+// End enum section
+
+// Start union section
+
+
+// End union section
+
+// End namespace example
+// Namespace start example
+
+// Start typedef section
+// End typedef section
+
+// Start struct section
+exports.Foo = Foo;
+function Foo() {
+    return new _xdrJsSerialize.default.Struct(
+        ["status","one","two","three",],
+        [FooStatus(),new _xdrJsSerialize.default.Str('', 256),new _xdrJsSerialize.default.Str('', 256),new _xdrJsSerialize.default.Str('', 256),]
+    )
+}
+
+// End struct section
+
+// Start enum section
+
+exports.FooStatus = FooStatus;
+function FooStatus() {
+    return new _xdrJsSerialize.default.Enum({
+        0: "Zero",
+        1: "One",
+        2: "Two",
+        3: "Three",
+        
+    })
+}
+
+
+// End enum section
+
+// Start union section
+
+
+// End union section
+
+// End namespace example
